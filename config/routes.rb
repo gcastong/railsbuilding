145ed216e9459290aa1382666aeb8a68fdd78685
Guillaume Castonguay <guillaume.castonguay@gmail.com>
2013-07-19 16:20:14 -0400

    Updated to add Room module

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Building::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
-
-  # Example of regular route:
+  root to: "welcome#index"  
+  resources :rooms 
+  GET 'rooms/:id(.:format)' => 'room#show'
+  GET 'rooms(.:format)' => 'room#index'
+# Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
